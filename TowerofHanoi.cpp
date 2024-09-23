76a194653f54e9247bbfea79a9183676f5d60de1
Nataly <nataulochka@gmail.com>
2024-09-23 20:29:23 +0300

    Added a task about the Tower of Hanoi

---
+++ TowerofHanoi.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+void TowerOfHanoi(int n, int i, int k)
+{
+	if (n == 1)
+	{
+		std::cout << "Переложить диск 1 cо стержня " << i << " на стержень " << k << std::endl;
+	}
+	else
+	{
+
+		int j = 6 - i - k; // Номер побочного столбика
+		TowerOfHanoi(n - 1, i, j);
+		std::cout << "Переложить диск " << n << " со стержня " << i << " на стержень " << k << std::endl;
+		TowerOfHanoi(n - 1, j, k);
+	}
+}
+
+int main()
+{
+	std::cout << "Ввудите количество дисков: ";
+	int n;
+	std::cin >> n;
+	std::cout << "Введите номер, на котором находятся диски: ";
+	int i;
+	std::cin >> i;
+	std::cout << "Введите номер стержня, на который нужно переложить диски: ";
+	int k;
+	std::cin >> k;
+
+	TowerOfHanoi(n, i, k);
+
+	return 0;
+}
