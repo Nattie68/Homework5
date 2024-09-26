7b88d82a1b6f96b8e94cc1c06fc76c3a5e72237d
Nataly <nataulochka@gmail.com>
2024-09-26 16:48:19 +0300

    code has been reworked

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,42 +1,30 @@
+
 #include <iostream>
-#include <string>
-// i - строки
-//  j - столбцы
-//  k - уменьшение количества зануляемых элементов строке
-//  line - номер строки, которую умножаем на koef
-// l - перебираем столбцы матрицы
-double all_koef{1};
 
-int det(int j, int k, int n, double *parr, int line, int check)
+double det(double *parr, int n)
 {
-    double koef;
-    for (int i = 1; i <= n - 1 - check; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        double elem_1 = parr[n * i + j];
-        double elem_2 = parr[n * (i - 1) + j];
-        koef = elem_2 / elem_1;
-        all_koef = all_koef * koef;
-
-        for (int l = 0; l < n; ++l)
+        for (int j = i + 1; j < n; ++j)
         {
-            parr[n * line + l] = parr[n * line + l] * koef;
+            double koef = parr[j * n + i] / parr[i * n + i];
+            for (int line = i; line < n; ++line)
+            {
+                parr[j * n + line] = parr[j * n + line] - koef * parr[i * n + line];
+            }
         }
-        for (int l = n - 1; l >= 0; --l)
-        {
-            parr[l + n * (line - 1)] = parr[l + n * (line - 1)] - parr[l + n * line];
-        }
-        ++line;
     }
-    return 0;
+    double det = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        det = det * parr[i * n + i];
+    }
+    return det;
 }
 
 int main()
 {
-    int k = 0;
-    int line = 1;
-    int check = 0;
     int n;
-    double d = 1;
     std::cin >> n;
     double *parr = new double[n * n]{};
 
@@ -47,17 +35,8 @@ int main()
             std::cin >> parr[n * i + j];
         }
     }
-    for (int j = n - 1; j > 0; --j)
-    {
 
-        det(j, k, n, parr, line, check);
-        ++k;
-        ++check;
-    }
-    for (int z = 0; z < n; ++z)
-    {
-        d = d * parr[(n + 1) * z];
-    }
-    std::cout << d / all_koef;
+    std::cout << det(parr, n);
+    delete[] parr;
     return 0;
 }
